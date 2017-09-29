2bc3cb7bee8e26e60cb792f68a04f24806763c56
ZiulLopes <lopesluiz_@hotmail.com>
2017-09-29 12:51:46 -0300

    Ultima atualização com GCH

--- knn_estudo.py
+++ knn_estudo.py
@@ -32,10 +32,29 @@ def testImg(image):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     kps, descs = sift.detectAndCompute(gray, None)
     return [descs[0]]
+
+def testImgGCH(image):
+    img = cv2.imread("test/{}".format(image))
+    rows, cols, channels = img.shape
+    histogram_b = np.zeros(4)
+    histogram_g = np.zeros(4)
+    histogram_r = np.zeros(4)
+
+    for row in range(rows):
+        for col in range(cols):
+            pixel_b = img[row, col, 0] / 64
+            pixel_g = img[row, col, 1] / 64
+            pixel_r = img[row, col, 2] / 64
+            histogram_b[int(pixel_b)] = histogram_b[int(pixel_b)] + 1
+            histogram_g[int(pixel_g)] = histogram_g[int(pixel_g)] + 1
+            histogram_r[int(pixel_r)] = histogram_r[int(pixel_r)] + 1
+
+    vector_features = np.append(histogram_b, histogram_g)
+    return [np.append(vector_features, histogram_r)]
     
 
 # Dataset feito de forma manual
-X2 = ds.DataSet.load_data()
+X2 = ds.DataSet.dataset_load_horse_cow()
 Y2 = ds.DataSet.load_target()
 
 knn = KNeighborsClassifier(n_neighbors=5, metric = "euclidean")
@@ -45,4 +64,4 @@ pathTest = r"""C:\Projects\python_projects\MachineLearning\KNN\test"""
 tests = os.listdir(pathTest)
 
 for img in tests:
-    print("{}|{}".format(img, returnAnimal(testImg(img))))
+    print("{}|{}".format(img, returnAnimal(testImgGCH(img))))
